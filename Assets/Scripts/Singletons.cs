f6142980cdbdcb7aa3da9dbbabcd954c7338bd3d
gcwhitfield <gwhitfieldjr@gmail.com>
2021-09-12 16:35:31 -0400

    Add Singletons.cs

--- Assets/Scripts/Singletons.cs
+++ Assets/Scripts/Singletons.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singletons : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : Component
 {
-    // Start is called before the first frame update
-    void Start()
+    protected static T instance;
+    public static T Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    instance = Instantiate(new GameObject()).AddComponent<T>();
+                    instance.hideFlags = HideFlags.HideAndDontSave;
+                }
+            }
+            return instance;
+        }
     }
+}
 
-    // Update is called once per frame
-    void Update()
+public class SingletonPersistent<T> : Singleton<T> where T : Component
+{
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {
+            Destroy(this);
+        }
     }
 }
