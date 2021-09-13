939aa8aeba30db5818b2947cb1c8b4d15c75cc7d
Charles Hood <chood@andrew.cmu.edu>
2021-09-13 01:42:51 -0400

    Position staged tiles side by side

--- Assets/Scripts/Spelling/GraphemeTile.cs
+++ Assets/Scripts/Spelling/GraphemeTile.cs
@@ -8,6 +8,6 @@ public class GraphemeTile : MonoBehaviour
 
     void OnMouseDown()
     {
-        spellingController.ActivateTile(this);
+        spellingController.ActivateTile(gameObject);
     }
 }
--- Assets/Scripts/Spelling/GraphemeTileInventory.cs
+++ Assets/Scripts/Spelling/GraphemeTileInventory.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public interface ISpellingController
 {
-    void ActivateTile(GraphemeTile tile);
+    void ActivateTile(GameObject tile);
 }
 
 public class GraphemeTileInventory : MonoBehaviour, ISpellingController
 {
     public GameObject graphemeTilePrefab;
-    public Vector3 graphemeTileStagingPosition;
+    public Vector3 stagedTilePosition;
 
     const int rowCount = 4;
-    GameObject[,] tiles = new GameObject[rowCount, rowCount];
+    GameObject[,] tileTable = new GameObject[rowCount, rowCount];
+    const float stagedTileSpacing = 1.125f;
+    List<GameObject> stagedTiles = new List<GameObject>();
 
     void Start()
     {
@@ -23,16 +25,27 @@ public class GraphemeTileInventory : MonoBehaviour, ISpellingController
         for (int y = 0; y < rowCount; ++y) {
             for (int x = 0; x < rowCount; ++x) {
                 Vector3 position = bottomLeft + spacing * new Vector3(x, y, 0.0f);
-                var t = Instantiate(graphemeTilePrefab, position, Quaternion.identity);
-                t.GetComponent<GraphemeTile>().spellingController = this;
-                tiles[y, x] = t;
+                var tile = Instantiate(graphemeTilePrefab, position, Quaternion.identity);
+                tile.GetComponent<GraphemeTile>().spellingController = this;
+                tileTable[y, x] = tile;
             }
         }
     }
 
-    public void ActivateTile(GraphemeTile tile)
+    public void ActivateTile(GameObject tile)
     {
-        Debug.Log("Tile activated");
-        tile.transform.position = graphemeTileStagingPosition;
+        stagedTiles.Add(tile);
+        ArrangeStagedTiles();
+    }
+
+    void ArrangeStagedTiles()
+    {
+        float width = stagedTileSpacing * (stagedTiles.Count - 1);
+        Vector3 left = stagedTilePosition - new Vector3(width * 0.5f, 0.0f, 0.0f);
+
+        for (int i = 0; i < stagedTiles.Count; ++i) {
+            GameObject tile = stagedTiles[i];
+            tile.transform.position = left + stagedTileSpacing * new Vector3(i, 0.0f, 0.0f);
+        }
     }
 }
