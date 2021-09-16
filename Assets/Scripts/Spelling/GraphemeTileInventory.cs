6b528abab64da197ef86b47ba09ee2fb9289f2e5
Charles Hood <chood@andrew.cmu.edu>
2021-09-16 00:45:04 -0400

    Use rectangular 5x3 grid for tiles

--- Assets/Scripts/Spelling/GraphemeTileInventory.cs
+++ Assets/Scripts/Spelling/GraphemeTileInventory.cs
@@ -7,34 +7,56 @@ public interface ISpellingController
     void ActivateTile(GameObject tile);
 }
 
-public class GraphemeTileInventory : MonoBehaviour, ISpellingController
+public class GraphemicInventory
 {
-    public GameObject graphemeTilePrefab;
-    public Vector3 stagedTilePosition;
-
-    List<string> graphemes = new List<string> {
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-    };
-    Dictionary<string, int> graphemeRatings = new Dictionary<string, int> {
+    Dictionary<string, int> graphemePoints = new Dictionary<string, int> {
         { "a", 1 }, { "b", 3 }, { "c", 2 }, { "d", 2 }, { "e", 1 }, { "f", 2 },
         { "g", 3 }, { "h", 2 }, { "i", 1 }, { "j", 4 }, { "k", 3 }, { "l", 2 },
         { "m", 2 }, { "n", 1 }, { "o", 1 }, { "p", 3 }, { "q", 4 }, { "r", 1 },
         { "s", 1 }, { "t", 1 }, { "u", 2 }, { "v", 3 }, { "w", 3 }, { "x", 4 },
         { "y", 3 }, { "z", 4 },
     };
-    const int rowCount = 4;
+    List<string> graphemeList;
+
+    public GraphemicInventory()
+    {
+        graphemeList = new List<string>(graphemePoints.Keys);
+    }
+
+    public string GetRandomGrapheme()
+    {
+        return graphemeList[Random.Range(0, graphemeList.Count)];
+    }
+
+    public int ScoreWord(List<string> graphemeList)
+    {
+        int score = 0;
+        foreach (string grapheme in graphemeList) {
+            score += graphemePoints[grapheme];
+        }
+        return score;
+    }
+}
+
+public class GraphemeTileInventory : MonoBehaviour, ISpellingController
+{
+    public GameObject graphemeTilePrefab;
+    public Vector3 stagedTilePosition;
+
+    GraphemicInventory graphemicInventory = new GraphemicInventory();
+    const int columnCount = 3;
+    const int rowCount = 5;
     const float transitionSpeed = 0.5f;
-    GameObject[,] tileTable = new GameObject[rowCount, rowCount];
+    GameObject[,] tileTable = new GameObject[columnCount, rowCount];
     const float stagedTileSpacing = 1.125f;
     List<GameObject> stagedTiles = new List<GameObject>();
 
     void Start()
     {
-        for (int y = 0; y < rowCount; ++y) {
+        for (int y = 0; y < columnCount; ++y) {
             for (int x = 0; x < rowCount; ++x) {
                 var tile = Instantiate(graphemeTilePrefab, Vector3.zero, Quaternion.identity);
-                tile.GetComponent<GraphemeTile>().Initialize(this, graphemes[Random.Range(0, graphemes.Count)]);
+                tile.GetComponent<GraphemeTile>().Initialize(this, graphemicInventory.GetRandomGrapheme());
                 tileTable[y, x] = tile;
             }
         }
@@ -64,38 +86,31 @@ public class GraphemeTileInventory : MonoBehaviour, ISpellingController
         var spelledString = string.Join("", spelledGraphemes);
 
         if (GetComponent<Lexicon>().Includes(spelledString)) {
-            Debug.Log("Score of '" + spelledString + "' is " + ScoreWord(spelledGraphemes));
+            Debug.Log("Score of '" + spelledString + "' is " + graphemicInventory.ScoreWord(spelledGraphemes));
         }
     }
 
     void PositionTiles()
     {
         float width = transform.localScale.x;
-        float spacing = width / (rowCount - 1);
-        Vector3 tableBottomLeft = transform.position - new Vector3(width, width, 0.0f) * 0.5f;
-        for (int y = 0; y < rowCount; ++y) {
+        float height = transform.localScale.y;
+        float horizontalSpacing = width / (rowCount - 1);
+        float verticalSpacing = height / (columnCount - 1);
+        Vector3 tableBottomLeft = transform.position - new Vector3(width, height, 0.0f) * 0.5f;
+        for (int y = 0; y < columnCount; ++y) {
             for (int x = 0; x < rowCount; ++x) {
-                Vector3 position = tableBottomLeft + spacing * new Vector3(x, y, 0.0f);
+                Vector3 position = tableBottomLeft + new Vector3(horizontalSpacing * x, verticalSpacing * y, 0.0f);
                 GameObject tile = tileTable[y, x];
                 tile.GetComponent<GraphemeTile>().Position(position);
             }
         }
 
-        float stagedWidth = spacing * (stagedTiles.Count - 1);
+        float stagedWidth = horizontalSpacing * (stagedTiles.Count - 1);
         Vector3 stagedLeft = stagedTilePosition - new Vector3(stagedWidth * 0.5f, 0.0f, 0.0f);
         for (int i = 0; i < stagedTiles.Count; ++i) {
-            Vector3 position = stagedLeft + spacing * new Vector3(i, 0.0f, 0.0f);
+            Vector3 position = stagedLeft + horizontalSpacing * new Vector3(i, 0.0f, 0.0f);
             GameObject tile = stagedTiles[i];
             tile.GetComponent<GraphemeTile>().Position(position);
         }
-    }
-
-    int ScoreWord(List<string> graphemeList)
-    {
-        int rating = 0;
-        foreach (string grapheme in graphemeList) {
-            rating += graphemeRatings[grapheme];
-        }
-        return rating;
     }
 }
