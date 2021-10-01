61757879fce444dd8418fc1bc593e86462dfa0f7
0-0-256 <cheng.cameron@gmail.com>
2021-09-30 22:01:29 -0400

    made new branch; silenced test SFX

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -22,23 +22,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(TempPlsDeleteLater());
+    
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-    
-    IEnumerator TempPlsDeleteLater()
-    {
-        yield return new WaitForSeconds(2.5F);
-        while (true)
-        {
-            Attack();
-            yield return new WaitForSeconds(5);
-        }
     }
     
     public void Attack()
