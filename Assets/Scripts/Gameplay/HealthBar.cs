f4a71c6043b89981acef971d7b0d2c3fdc10badc
Charles Hood <chood@andrew.cmu.edu>
2021-10-24 12:29:40 -0400

    Create healthbar using art asset

---
+++ Assets/Scripts/Gameplay/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private GameObject mask;
+    private Vector3 maskInitialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mask = this.transform.Find("Mask").gameObject;
+        this.maskInitialPosition = this.mask.transform.position;
+
+        this.SetHealth(0.01f);
+    }
+
+    void SetHealth(float proportion)
+    {
+        var healthBarWidth = 3.0625f;
+        this.mask.transform.position = this.maskInitialPosition + Vector3.left * (1.0f - proportion) * healthBarWidth;
+    }
+}
