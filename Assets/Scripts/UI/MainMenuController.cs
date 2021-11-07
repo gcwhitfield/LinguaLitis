d3d01c53dd2b9f2e3c88c6209a205be3cb845a53
gcwhitfield <gwhitfieldjr@gmail.com>
2021-11-07 16:29:57 -0500

    Added sounds to the main menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : UnitySingleton<MainMenuController>
     [Header("Start Button")]
     public string gameplaySceneName = "Gameplay";
 
-    // called when the "Start" button on the main menu screen gets pressed
+    // plays a sound, transitions to next scene when "Start" button on the main menu screen gets pressed
     public void StartButtonPressed()
     {
         FMOD.Studio.EventInstance evt;
@@ -15,5 +15,24 @@ public class MainMenuController : UnitySingleton<MainMenuController>
         evt.start();
         evt.release();
         SceneTransitionManager.Instance.TransitionToScene(gameplaySceneName);
+    }
+
+    // plays sound when the cursor hovers over a main menu button
+    public void OnButtonHovered()
+    {
+        FMOD.Studio.EventInstance evt;
+        evt = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/UI/ButtonHover");
+        evt.start();
+        evt.release();
+    }
+
+    // plays sound when any of the main menu buttons are pressed (other than the start button, which
+    // has its own special sound)
+    public void OnButtonPressed()
+    {
+        FMOD.Studio.EventInstance evt;
+        evt = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/UI/ButtonPress");
+        evt.start();
+        evt.release();
     }
 }
