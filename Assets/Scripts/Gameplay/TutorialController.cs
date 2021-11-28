de2376b3ce6feccbeb98a264c28be14d6d6ee6ac
gcwhitfield <gwhitfieldjr@gmail.com>
2021-11-28 15:28:50 -0500

    Update tutorial scene. Add sound credits to credits.txt

--- Assets/Scripts/Gameplay/TutorialController.cs
+++ Assets/Scripts/Gameplay/TutorialController.cs
@@ -6,6 +6,8 @@ public class TutorialController : UnitySingleton<TutorialController>
 {
     [Tooltip("The name of the next scene to transition to")]
     public string nextScene;
+    public GameObject[] screens;
+    int currScreenIndex = 0;
 
     void Start()
     {
@@ -13,11 +15,33 @@ public class TutorialController : UnitySingleton<TutorialController>
         {
             SceneTransitionManager.Instance.sceneTransitionAnimator.SetTrigger("Open");
         }
+        screens[currScreenIndex].SetActive(true);
+    }
+
+    // show the currently active tutorial screen
+    void ShowScreen()
+    {
+        foreach (GameObject s in screens)
+        {
+            s.SetActive(false);
+        }
+        screens[currScreenIndex].SetActive(true);
     }
 
     void Update()
     {
-        if (Input.anyKeyDown)
+        // listen for inputs from player. increment or decrement current screen based on keyboard input
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            currScreenIndex++;
+            ShowScreen();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            currScreenIndex--;
+            ShowScreen();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             SceneTransitionManager.Instance.TransitionToScene(nextScene);
         }
